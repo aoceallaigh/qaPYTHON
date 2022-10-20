051134b9b77b29859e053e80c4e34edaf7adb6ee
aoceallaigh <andrew.oceallaigh@ucdconnect.ie>
2022-10-20 09:30:43 +0100

    added seaborn capability

--- reader.py
+++ reader.py
@@ -1,4 +1,16 @@
 import pandas as pd
+import numpy as np
+import seaborn as sb
+import matplotlib.pyplot as plt 
+
 
 df = pd.read_csv("test.csv")
-print(df.corr())
+print(df.head())
+print(df.describe())
+
+newplot = sb.scatterplot(
+    x = "Weight",
+    y = "Age",
+    data = df 
+)
+plt.show()
